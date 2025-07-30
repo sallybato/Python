1ea59535207764949063db6feba9ee862ce0d305
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-07-30 11:32:41 -0300

    Create aula.py

---
+++ aula.py
@@ -0,0 +1,8 @@
+metros=int(input("Digite o valor em metros"))
+decimetros = metros *10
+centimetros = metros *100
+milimetros = metros *1000
+
+print("decimetros: ",decimetros)
+print("centimtros: ",centimetros)
+print("milimetros: ",milimetros)
