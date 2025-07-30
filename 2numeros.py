a596d765d4e4fd1138281cab64895576f13105f7
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-07-30 11:33:46 -0300

    Create 2numeros.py

---
+++ 2numeros.py
@@ -0,0 +1,20 @@
+import math
+
+num1 = int(input("Digite o numero 1:"))
+num2 = int(input("Digite o numero 2:"))
+
+soma = (num1+num2)
+print("A soma é >", soma)
+
+produto = num1 * (num2 * num2)
+print("O produto é >",produto)
+
+quadrado = (num1 * num1)
+print("O quadrado é >",quadrado)
+
+raiz_quadrada = math.sqrt(num1**2 + num2**2)
+print("A raiz é >",raiz_quadrada)
+
+sen = math.sin(num1 - num2)
+print("A raiz é >", sen)
+
