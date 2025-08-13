1bab06348be93f684f8ccc88c981d44cd886765f
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-08-13 11:12:19 -0300

    Create list_count.py

---
+++ list_count.py
@@ -0,0 +1,14 @@
+nome = ['rebecca','matheus','joao','arthur','bruna','joao','rebecca','rebecca','matheus']
+
+contagem_nomes = {
+    'rebecca': nome.count('rebecca'),
+    'matheus': nome.count('matheus'),
+    'joao': nome.count('joao'),
+    'arthur': nome.count('arthur'),
+    'bruna': nome.count('bruna')
+}
+print("O nome rebecca aparece", contagem_nomes['rebecca'], "vezes\n")
+print("O nome matheus aparece", contagem_nomes['matheus'], "vezes\n")
+print("O nome joao aparece", contagem_nomes['joao'], "vezes\n")
+print("O nome arthur aparece", contagem_nomes['arthur'], "vezes\n")
+print("O nome bruna aparece", contagem_nomes['bruna'], "vezes\n")
