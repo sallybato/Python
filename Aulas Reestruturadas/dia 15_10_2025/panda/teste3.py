be169baa34a3e575f502cfb5c67d923da55cc96a
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-10-15 11:22:50 -0300

    Create teste3.py

---
+++ Aulas Reestruturadas/dia 15_10_2025/panda/teste3.py
@@ -0,0 +1,21 @@
+import pandas as pd
+dados=pd.read_csv("dados.csv")
+print("=== DADOS ORIGINAIS ===")
+print(dados)
+print("\n")
+#verificar valores nulos
+print("=== VERIFICANDO VALORES FALTANTES ===")
+print(dados.isnull())
+print("\n")
+print("=== CONTAGEM DE VALORES FALTANTES ===")
+print(dados.isnull().sum())
+print("\n")
+#substituir valores nulos
+dados["Idade"] = dados["Idade"].fillna(dados["Idade"].mean())
+dados["Nota"] = dados["Nota"].fillna(dados["Nota"].mean())
+print("=== DADOS CORRIGIDOS ===")
+print(dados)
+print("\n")
+
+dados.to_csv("dados_corrigidos.csv", index=False)
+print("Arquivo 'dados_corrigidos.csv' salvo com sucesso!")
